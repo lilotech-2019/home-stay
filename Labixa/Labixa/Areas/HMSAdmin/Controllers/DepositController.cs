cf394e8df2cd6539567824c06cea0acc162225c4
ThanhPTP <ThanhPTP@fsoft.com.vn>
2019-04-19 23:44:43 +0700

    1/ Modified DepositController, ColorServices and Shared/_AdminLayout (Change route using @Url.Action) 2/ Add DepositViewCRUD 3/ Add Annotation to Colors Model

--- Labixa/Labixa/Areas/HMSAdmin/Controllers/DepositController.cs
+++ Labixa/Labixa/Areas/HMSAdmin/Controllers/DepositController.cs
@@ -1,73 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using Outsourcing.Data.Models;
+using Outsourcing.Service;
 using System.Data.Entity;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Net;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
-using Outsourcing.Data;
-using Outsourcing.Data.Models;
 
 namespace Labixa.Areas.HMSAdmin.Controllers
 {
     public class DepositController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        #region Fields
+        private readonly IColorService _colorService;
+        #endregion
 
-        // GET: /HMSAdmin/Deposit/
+        #region Ctor
+        public DepositController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }
+        #endregion
+
+        #region Index
+        /// <summary>
+        /// Index - GET
+        /// </summary>
+        /// <returns></returns>
         public async Task<ActionResult> Index()
         {
-            return View(await db.Colors.ToListAsync());
+            var colors = await _colorService.FindAll().AsNoTracking().ToListAsync();
+            return View(colors);
         }
+        #endregion
 
-        // GET: /HMSAdmin/Deposit/Details/5
-        public async Task<ActionResult> Details(int? id)
+        #region Details
+        /// <summary>
+        /// Details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Colors colors = await db.Colors.FindAsync(id);
+            Colors colors = _colorService.FindById((int)id);
             if (colors == null)
             {
                 return HttpNotFound();
             }
             return View(colors);
         }
+        #endregion
 
-        // GET: /HMSAdmin/Deposit/Create
+        #region Create
+        /// <summary>
+        /// Create - GET
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: /HMSAdmin/Deposit/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Create - POST
+        /// </summary>
+        /// <param name="colors"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include="Id,Name,Email,Description,Content,Note,isDelete")] Colors colors)
+        public ActionResult Create(Colors colors)
         {
             if (ModelState.IsValid)
             {
-                db.Colors.Add(colors);
-                await db.SaveChangesAsync();
+                _colorService.Create(colors);
                 return RedirectToAction("Index");
             }
 
             return View(colors);
         }
+        #endregion
 
-        // GET: /HMSAdmin/Deposit/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        #region Edit
+        /// <summary>
+        /// Edit - GET
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Colors colors = await db.Colors.FindAsync(id);
+            Colors colors = _colorService.FindById((int)id);
             if (colors == null)
             {
                 return HttpNotFound();
@@ -75,30 +102,37 @@ namespace Labixa.Areas.HMSAdmin.Controllers
             return View(colors);
         }
 
-        // POST: /HMSAdmin/Deposit/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Edit - POST
+        /// </summary>
+        /// <param name="colors"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include="Id,Name,Email,Description,Content,Note,isDelete")] Colors colors)
+        public ActionResult Edit(Colors colors)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(colors).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                _colorService.Edit(colors);
                 return RedirectToAction("Index");
             }
             return View(colors);
         }
+        #endregion
 
-        // GET: /HMSAdmin/Deposit/Delete/5
-        public async Task<ActionResult> Delete(int? id)
+        #region Delete
+        /// <summary>
+        /// Delete - GET
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Colors colors = await db.Colors.FindAsync(id);
+            var colors = _colorService.FindById((int)id);
             if (colors == null)
             {
                 return HttpNotFound();
@@ -106,24 +140,18 @@ namespace Labixa.Areas.HMSAdmin.Controllers
             return View(colors);
         }
 
-        // POST: /HMSAdmin/Deposit/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(int id)
         {
-            Colors colors = await db.Colors.FindAsync(id);
-            db.Colors.Remove(colors);
-            await db.SaveChangesAsync();
+            var colors = _colorService.FindById(id);
+            if (colors == null)
+            {
+                return HttpNotFound();
+            }
+            _colorService.Delete(colors);
             return RedirectToAction("Index");
         }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        #endregion
     }
 }
