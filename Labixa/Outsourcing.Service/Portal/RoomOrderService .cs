28f68867caf68dde31df12301e1f6faa2da5b5f1
ThanhPTP <ThanhPTP@fsoft.com.vn>
2019-04-23 19:22:54 +0700

    1/ Modified RoomOrderService 2/ Modified Create,Edit and Index view of RoomOrderController

---
+++ Labixa/Outsourcing.Service/Portal/RoomOrderService .cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Outsourcing.Data.Infrastructure;
+using Outsourcing.Data.Models;
+using Outsourcing.Data.Models.HMS;
+using Outsourcing.Service.Portal.Base;
+
+namespace Outsourcing.Service.Portal
+{
+    public interface IRoomOrderService : IServiceBase<RoomOrder>
+    {
+    }
+
+    public class RoomOrderService : ServiceBase<RoomOrder>, IRoomOrderService
+    {
+        #region Ctor
+
+        public RoomOrderService(IRepository<RoomOrder> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+
+        #endregion
+
+        #region BaseMethod
+
+        #endregion
+    }
+}
