677cd85e3f214e0072979e306921b15fd423d165
Trần Hoàng Hải <haith2207@gmail.com>
2019-04-26 00:39:56 +0700

    fix code load asset for client

--- Labixa/Labixa/ViewModels/RoomVer3ViewModel.cs
+++ Labixa/Labixa/ViewModels/RoomVer3ViewModel.cs
@@ -9,15 +9,11 @@ namespace Labixa.ViewModels
 {
     public class RoomDetailViewModel
     {
-        
-
         public IEnumerable<Room> RelatedRooms { get; set; }
 
         //public IEnumerable<Rooms> RelatedRoomLong { get; set; }
 
         public Room Room { get; set; }
-
-
-
+        public IEnumerable<RoomAsset> RoomAssets { get; set; }
     }
 }
--- Labixa/Outsourcing.Service/Portal/RoomAssetService.cs
+++ Labixa/Outsourcing.Service/Portal/RoomAssetService.cs
@@ -1,4 +1,6 @@
-﻿using Outsourcing.Data.Infrastructure;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Outsourcing.Data.Infrastructure;
 using Outsourcing.Data.Models;
 using Outsourcing.Service.Portal.Base;
 
@@ -6,6 +8,8 @@ namespace Outsourcing.Service.Portal
 {
     public interface IRoomAssetService : IServiceBase<RoomAsset>
     {
+        IEnumerable<RoomAsset> FindByRoomIdAndAssetId(int roomId, int assetId);
+        IEnumerable<RoomAsset> FindByRoomId(int roomId);
     }
 
     public class RoomRoomAssetService : ServiceBase<RoomAsset>, IRoomAssetService
@@ -13,6 +17,16 @@ namespace Outsourcing.Service.Portal
         public RoomRoomAssetService(IRepository<RoomAsset> repository, IUnitOfWork unitOfWork) : base(repository,
             unitOfWork)
         {
+        }
+
+        public IEnumerable<RoomAsset> FindByRoomIdAndAssetId(int roomId, int assetId)
+        {
+            return Repository.FindBy(w => w.AssetId == assetId && w.RoomId == roomId && w.IsAvaiable).ToList();
+        }
+
+        public IEnumerable<RoomAsset> FindByRoomId(int roomId)
+        {
+            return Repository.FindBy(w => w.RoomId == roomId && w.IsAvaiable).ToList();
         }
     }
 }
