415a40a575a088eac360b10c91df92438980bace
HanhTT <phithanh7430@gmail.com.vn>
2019-05-18 18:27:41 +0700

    1. update icon left menu

---
+++ Labixa/Outsourcing.Data/Models/BlogImage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Outsourcing.Data.Models
+{
+    public class BlogImages : BaseEntity
+    {
+        public string Binary { get; set; }
+        public string MimeType { get; set; }
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+        public virtual Picture Picture { get; set; }
+        public virtual ICollection<BlogImageMappings> BlogImageMapping { get; set; }
+    }
+}
+
