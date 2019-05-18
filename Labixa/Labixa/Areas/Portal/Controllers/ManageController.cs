fa41f05b119e7fdd65f8a700b587b877862d3dc0
Trần Hoàng Hải <haith2207@gmail.com>
2019-05-18 18:12:11 +0700

    manage user

---
+++ Labixa/Labixa/Areas/Portal/Controllers/ManageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Labixa.Areas.Portal.ViewModels.User;
+using Outsourcing.Data;
+
+namespace Labixa.Areas.Portal.Controllers
+{
+    public class ManageController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        //
+        // GET: /Portal/Manage/
+        public ActionResult Index()
+        {
+            var users = (from user in _context.Users
+                select new
+                {
+                    UserId = user.Id,
+                    Username = user.UserName,
+                    Email = user.Email,
+                    RoleNames = (from userRole in user.Roles
+                        join role in _context.Roles on userRole.RoleId
+                            equals role.Id
+                        select role.Name).ToList()
+                }).ToList().Select(p => new UserIndexViewModels()
+
+            {
+                UserId = p.UserId,
+                Username = p.Username,
+                Email = p.Email,
+                Role = string.Join(",", p.RoleNames)
+            });
+            return View(users);
+        }
+    }
+}
