0dacb0e12f931a54e2a4f6b596780dfc21b9714c
ThanhPTP <ThanhPTP@fsoft.com.vn>
2019-05-20 21:33:20 +0700

    1/ Modified Hotels Details 2/ Clean Code

---
+++ Labixa/Labixa/Areas/Portal/ViewModels/Hotels/HotelDetailsSubMenuViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Labixa.Areas.Portal.ViewModels.Hotels
+{
+    public class HotelDetailsSubMenuViewModel
+    {
+        public PagedList.IPagedList<Outsourcing.Data.Models.Room> Rooms { get; set; }
+        public int HotelId { get; set; }
+
+    }
+}
